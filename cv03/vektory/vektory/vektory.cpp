f0aae256762579e724f92acbe52a433ab190f96d
David Sindelar <44811194+cryptonhd2004@users.noreply.github.com>
2024-10-02 08:01:54 +0200

    Add files via upload

---
+++ cv03/vektory/vektory/vektory.cpp
@@ -0,0 +1,26 @@
+﻿// vektory.cpp : Defines the entry point for the application.
+//
+
+#include "vektory.h"
+
+using namespace std;
+
+int main()
+{
+	float ux;
+	float uy;
+	float vx;
+	float vy;
+	float x;
+	float y;
+	cout << "Zadejte souradnice vektoru u (oddelte je mezerou): ";
+	cin >> ux;
+	cin >> uy;
+	cout << "\nZadejte souradnice vektoru v (oddelte je mezerou): ";
+	cin >> vx;
+	cin >> vy;
+	x = ux + vx;
+	y = uy + vy;
+	cout << "\nSoucet vektoru uv je: (" << x << "," << y << ").";
+	return 0;
+}
