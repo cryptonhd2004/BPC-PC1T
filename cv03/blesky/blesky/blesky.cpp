9c0ce6b887bdeec91959b6211097b1ad4b410bf6
David Sindelar <44811194+cryptonhd2004@users.noreply.github.com>
2024-10-02 07:46:10 +0200

    Add files via upload

---
+++ cv03/blesky/blesky/blesky.cpp
@@ -0,0 +1,20 @@
+﻿// blesky.cpp : Defines the entry point for the application.
+//
+
+#include "blesky.h"
+
+using namespace std;
+
+int main()
+{
+	float cas = 0;
+	float km = 0;
+	cout << "Zadejte cas ktery uplynul mezi bleskem a hromem:   ";
+	cin >> cas;
+	km = (cas * 340) / 1000;
+	cout << "Vase vzdalenost od blesku byla " << km << " km.\n";
+	cout << "Dekujeme za pouziti programu navidenou ;)";
+
+	//system("C:\\WINDOWS\\System32\\shutdown /s");
+	return 0;
+}
