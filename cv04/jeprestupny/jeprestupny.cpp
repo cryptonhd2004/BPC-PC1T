a558965b808dfd99b0ad5682ce3b741f8c4ff210
David Sindelar <44811194+cryptonhd2004@users.noreply.github.com>
2024-10-09 07:51:23 +0200

    Add files via upload

---
+++ cv04/jeprestupny/jeprestupny.cpp
@@ -0,0 +1,41 @@
+﻿// jeprestupny.cpp : Defines the entry point for the application.
+//
+
+#include "jeprestupny.h"
+
+using namespace std;
+
+
+int jePrestupny(int rok) {
+	if ((rok % 4 == 0 and rok % 100 != 0) || (rok % 400 == 0)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int jeLichy(int rok) {
+	if (rok % 2 != 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int main()
+{
+	int rok;
+	printf("|Rok    | Je prestupny  |  Je lichy	|\n");
+	int roky[7] = { 1000, 1999, 2000, 2002, 2022, 2024, 2200 };
+	for (int i = 0; i < 7; i++) {
+		rok = roky[i];
+		
+			printf("|%d	|	%d	|	%d	|\n", rok, jePrestupny(rok), jeLichy(rok));
+			
+	}
+
+
+	return 0;
+}
